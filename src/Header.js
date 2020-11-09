01649121b999af8fb055c046d08fe5548aa6f429
imakshat47 <imakshat@github.com>
2020-11-10 02:07:31 +0530

    First Commit

---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Header.css";
+function Header(props) {
+  return (
+    <div className="header">
+      <div className="header_left">
+          
+      </div>
+      <div className="header_search">
+          
+      </div>
+      <div className="header_right">
+          
+      </div>
+    
+    </div>
+  );
+}
+
+export default Header;
